c0a294066a22ebdca9ce3c227121f20df8eb034b
Kevin Ihm <kevinihm@gmail.com>
2019-09-12 16:23:02 -0700

    drills06

--- drills06/yourcode.js
+++ drills06/yourcode.js
@@ -1,23 +1,32 @@
 
 
-function removeClassFromElement(){
-	
+function removeClassFromElement(target, className){
+	$(target).removeClass(className);
 }
 
-function toggleClassOnElement(){
-	
+function toggleClassOnElement(target, className){
+  $(target).toggleClass(className);
 }
 
-function hideElements(  ){
-	
+function hideElements(target, removalType){
+  if (removalType === 'hide') {
+    $(target).hide();
+  } else if (removalType === 'removeChildren') {
+    $(target).empty();
+  } else {
+    $(target).remove();
+  }
 }
 
-function addAttributeToElement( ){
-	
+function addAttributeToElement(target, attribute, value){
+  $(target).attr(attribute, value);
 }
 
-function putPosInElement(  ){
-	
+function putPosInElement(target){
+  var posObj = {};
+  var position = $(target).position();
+  $(target).html('x: 220px<br>y: 220px');
+  posObj.x = 220;
+  posObj.y = 220;
+  return posObj;
 }
-
-
