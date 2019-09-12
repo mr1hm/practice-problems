54ea41a5231f1873cb853926b6f79988205d8ebd
Kevin Ihm <kevinihm@gmail.com>
2019-09-12 13:00:44 -0700

    drills04

--- drills04/yourcode.js
+++ drills04/yourcode.js
@@ -1,17 +1,45 @@
 
 
-function changeElements(){
-
+function changeElements(elementClass){
+  var classText = $(elementClass).text();
+  var numObj = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9
+  };
+  $(elementClass).each(function(index) {
+    var thisText = $(this).text();
+    console.log(thisText);
+    if (numObj[thisText]) {
+      $(this).text(numObj[thisText]);
+    }
+  })
 }
 
-function appendTextToElement(){
-
+function appendTextToElement(elementClass, word){
+  $(elementClass).each(function() {
+    var originalText = $(this).text();
+    console.log(originalText);
+    $(this).text(originalText + word);
+  })
 }
 
-function addClass(){
+function addClass(selector, classToAdd){
+  var count = 0;
+  $(selector).each(function() {
+    if($(this).addClass(classToAdd)) {
+      count++;
+    }
+  })
+  return count;
 }
 
-function removeElements(){
-
+function removeElements(elementToRemove){
+  $(elementToRemove).remove();
 }
-
